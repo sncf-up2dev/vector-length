5da523e065018d4692acf5143fd0ee9c3bbe8b63
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-02-20 15:06:43 +0100

    Anonymous class

--- src/main/java/fr/sncf/d2d/training/Main.java
+++ src/main/java/fr/sncf/d2d/training/Main.java
@@ -3,6 +3,17 @@ package fr.sncf.d2d.training;
 public class Main {
 
     public static void main(String[] args) {
+        var vector = new Object() {
+            double x;
+            double y;
 
+            double length() {
+                return Math.sqrt(x * x + y * y);
+            }
+        };
+
+        vector.x = 3;
+        vector.y = 4;
+        System.out.println(vector.length());
     }
 }
